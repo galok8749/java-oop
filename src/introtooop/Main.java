2960a6f3d3d4610b9f7cca9e0172309317fb1ad6
alok <galok8749@gmail.com>
2022-07-17 10:49:42 +0530

    OOP-1

---
+++ src/introtooop/Main.java
@@ -0,0 +1,18 @@
+package introtooop;
+
+public class Main {
+    public static void main(String[] args) {
+        Student kartik = new Student();
+        kartik.name = "Kartik";
+        kartik.address = "scalerverse";
+        kartik.state = "ACTIVE";
+        kartik.pauseCourse();
+        kartik.kartik();
+
+        Student naman = new Student();
+        naman.name = "Naman";
+        naman.batch = "Best atch of Scaler";
+        naman.kartik();
+        System.out.println("Doing Nothing");
+    }
+}
